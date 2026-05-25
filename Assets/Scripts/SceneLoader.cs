bf40ee140dad7d940438c7698ae80cf3d1f599c6
김남우 <rlaskadn02@naver.com>
2026-05-25 14:01:02 +0900

    Update

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,6 +1,6 @@
 /*
     최초 작성일:26/05/17
-    최종 변경일:26/05/17
+    최종 변경일:26/05/25
     
     수정자
     - 김남우
@@ -8,7 +8,7 @@
     
     목적
     - LoadingScene의 로직을 개발하기 위해 SceneName Enum의 관리와
-      Loading씬으로 이동하는 로직을 설계
+    - Loading씬으로 이동하는 로직을 설계
 */
 
 using UnityEngine;
